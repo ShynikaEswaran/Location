092f8bfd6b9d55f3945606582aba3550db1ff19d
Shynika@3298 <shyniswasthika@gmail.com>
2025-04-22 19:06:58 +0530

    code added

---
+++ Sample.java
@@ -0,0 +1,11 @@
+package org.test;
+
+public class Sample {
+	private void shyni() {
+System.out.println("Hi");
+	}
+public static void main(String[] args) {
+	System.out.println("123456");
+	
+}
+}
