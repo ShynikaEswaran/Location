683e14932eb225d3634bca6b9719a22d3d244ed7
Shynika@3298 <shyniswasthika@gmail.com>
2025-04-22 21:29:52 +0530

    code added

--- salary.java
+++ salary.java
@@ -4,7 +4,7 @@ public class salary {
 private void arun() {
 System.out.println("hai");
 }
-private void fruit() {
+private void vegetable() {
 System.out.println("hiiii");
 System.out.println("hello");
 }
