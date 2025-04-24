22bc644969354a9302ae3164bab38f5e508c5770
ShynikaMohanraja <beelinemohan@gmail.com>
2025-04-24 20:01:06 +0530

    new coded added by mohan

--- salary.java
+++ salary.java
@@ -5,8 +5,11 @@ private void arun() {
 System.out.println("hai");
 }
 private void vegetable() {
-System.out.println("hiiii");
+System.out.println("hiiiiiiiii");
 System.out.println("hello");
+System.out.println("hiiiii");
+System.out.println("hiiiiiiiiiiiiiiii");
+System.out.println("gewfgegs");
 }
 
 }
